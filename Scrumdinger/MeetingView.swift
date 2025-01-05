eff67c7ce750b20f75c309b53c03bde1adfe5997
Nicolas Bures <nick.bures@gmail.com>
2025-01-05 15:52:02 +0100

    add basic view

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        ProgressView(value: 10, total: 15)
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Second Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                VStack {
+                    Text("Second Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
     }
 }
 
