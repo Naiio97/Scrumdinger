93b8e3d26fe9a8bc2dbdf6036481dc2809ccc93a
Nicolas Bures <nick.bures@gmail.com>
2025-01-05 14:25:14 +0100

    rename ContetView on MeetingView

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Nicolas Bureš on 05.01.2025.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
