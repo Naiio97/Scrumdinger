35e56cab8b6532f3604c29b517aeb787819d3c24
Nicolas Bures <nick.bures@gmail.com>
2025-01-05 15:35:37 +0100

    rename ContetView on MeetingView

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,17 +9,11 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        ProgressView(value: 10, total: 15)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
         MeetingView()
     }
