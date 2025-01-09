5cfb49c72904ea450c3b42c38daa853f8b755397
Nicolas Bures <nick.bures@gmail.com>
2025-01-09 19:16:39 +0100

    add data

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -1,8 +1,27 @@
-//
-//  DailyScrum.swift
-//  Scrumdinger
-//
-//  Created by Nicolas Bureš on 09.01.2025.
-//
-
 import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lenghtInMinutes: Int
+    var theme: Theme
+}
+
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Desing",
+                   attendees: ["Cathey", "Daisy", "Simon", "Luke"],
+                   lenghtInMinutes: 10,
+                   theme: .yellow),
+        DailyScrum(title: "AppDev",
+                   attendees: ["Nicolas", "Maros", "Krystof", "Filip"],
+                   lenghtInMinutes: 5,
+                   theme: .orange),
+        DailyScrum(title: "Desing",
+                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+                   lenghtInMinutes: 5,
+                   theme: .poppy),
+    ]
+}
