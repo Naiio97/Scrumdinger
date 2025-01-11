f609bc45d59b4ac426d686095431d636bcd79c7f
Nicolas Bures <nick.bures@gmail.com>
2025-01-11 18:16:45 +0100

    add id, add list and build app with ScrumsView

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -1,10 +1,19 @@
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    var id: UUID
     var title: String
     var attendees: [String]
     var lenghtInMinutes: Int
     var theme: Theme
+    
+    init(id: UUID = UUID(), title: String, attendees: [String], lenghtInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lenghtInMinutes = lenghtInMinutes
+        self.theme = theme
+    }
 }
 
 
