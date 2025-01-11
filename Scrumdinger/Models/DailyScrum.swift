3ec9856f895acc5df54c2b1da5d2ac0f317bb8b1
Nicolas Bures <nick.bures@gmail.com>
2025-01-11 17:42:26 +0100

    create ScrumsView

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -11,15 +11,15 @@ struct DailyScrum {
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
-        DailyScrum(title: "Design",
+        DailyScrum(title: "Desing",
                    attendees: ["Cathey", "Daisy", "Simon", "Luke"],
                    lenghtInMinutes: 10,
                    theme: .seafoam),
-        DailyScrum(title: "AppDev",
+        DailyScrum(title: "App Dev",
                    attendees: ["Nicolas", "Maros", "Krystof", "Filip"],
                    lenghtInMinutes: 5,
                    theme: .orange),
-        DailyScrum(title: "Desing",
+        DailyScrum(title: "Web Dev",
                    attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
                    lenghtInMinutes: 5,
                    theme: .poppy),
