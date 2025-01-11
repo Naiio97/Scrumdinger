ce2b6abf17c9babbe5d89fb81f0296731b0f8630
Nicolas Bures <nick.bures@gmail.com>
2025-01-11 20:58:10 +0100

    add id for attendees, iterate through attendees

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,51 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Nicolas Bureš on 11.01.2025.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                    .accessibilityLabel("Start Meeting")
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lenghtInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")}
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            DetailView(scrum: DailyScrum.sampleData[1])
+        }
+    }
+}
--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -3,19 +3,30 @@ import Foundation
 struct DailyScrum: Identifiable {
     var id: UUID
     var title: String
-    var attendees: [String]
+    var attendees: [Attandee]
     var lenghtInMinutes: Int
     var theme: Theme
     
     init(id: UUID = UUID(), title: String, attendees: [String], lenghtInMinutes: Int, theme: Theme) {
         self.id = id
         self.title = title
-        self.attendees = attendees
+        self.attendees = attendees.map { Attandee(name: $0) }
         self.lenghtInMinutes = lenghtInMinutes
         self.theme = theme
     }
 }
 
+extension DailyScrum {
+    struct Attandee: Identifiable {
+        let id: UUID
+        var name: String
+        
+        init(id: UUID = UUID(), name:String) {
+            self.id = id
+            self.name = name
+        }
+    }
+}
 
 extension DailyScrum {
     static let sampleData: [DailyScrum] =
