fa6412fbdac5abf5f60a88ab548e6eee1fefd43a
Nicolas Bures <nick.bures@gmail.com>
2025-01-12 17:29:47 +0100

    add edit funcionality, add cancle and done button

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct DetailView: View {
-    var scrum: DailyScrum
+    let scrum: DailyScrum
+    
+    @State private var isPresentingEditView = false
     
     var body: some View {
         List {
@@ -43,6 +45,29 @@ struct DetailView: View {
             }
         }
         .navigationTitle(scrum.title)
+        .toolbar{
+            Button("Edit"){
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView){
+            NavigationStack {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar{
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancle"){
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
